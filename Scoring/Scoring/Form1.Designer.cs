d593add9af790978db860b2079ca47aa7fd876b3
Kenny York <kenny.york+github@gmail.com>
2011-02-25 16:31:18 -0600

    not sure

--- Scoring/Scoring/Form1.Designer.cs
+++ Scoring/Scoring/Form1.Designer.cs
@@ -28,12 +28,84 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnPrev = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.txtFirst = new System.Windows.Forms.TextBox();
+            this.txtLast = new System.Windows.Forms.TextBox();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnPrev
+            // 
+            this.btnPrev.Enabled = false;
+            this.btnPrev.Location = new System.Drawing.Point(12, 67);
+            this.btnPrev.Name = "btnPrev";
+            this.btnPrev.Size = new System.Drawing.Size(47, 23);
+            this.btnPrev.TabIndex = 0;
+            this.btnPrev.Text = "<";
+            this.btnPrev.UseVisualStyleBackColor = true;
+            this.btnPrev.Click += new System.EventHandler(this.btnPrev_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Enabled = false;
+            this.btnNext.Location = new System.Drawing.Point(119, 67);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(49, 23);
+            this.btnNext.TabIndex = 1;
+            this.btnNext.Text = ">";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // txtFirst
+            // 
+            this.txtFirst.Location = new System.Drawing.Point(12, 12);
+            this.txtFirst.Name = "txtFirst";
+            this.txtFirst.Size = new System.Drawing.Size(156, 20);
+            this.txtFirst.TabIndex = 2;
+            // 
+            // txtLast
+            // 
+            this.txtLast.Location = new System.Drawing.Point(12, 38);
+            this.txtLast.Name = "txtLast";
+            this.txtLast.Size = new System.Drawing.Size(156, 20);
+            this.txtLast.TabIndex = 3;
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(65, 67);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(47, 23);
+            this.btnEdit.TabIndex = 4;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(177, 96);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.txtLast);
+            this.Controls.Add(this.txtFirst);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnPrev);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnPrev;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.TextBox txtFirst;
+        private System.Windows.Forms.TextBox txtLast;
+        private System.Windows.Forms.Button btnEdit;
     }
 }
 
--- Scoring/Scoring/Form1.cs
+++ Scoring/Scoring/Form1.cs
@@ -11,9 +11,99 @@ namespace Scoring
 {
     public partial class Form1 : Form
     {
+        BindingList<Person> people = new BindingList<Person>();
+        Person activePerson = null;
+        int active = 0;
         public Form1()
         {
             InitializeComponent();
+            //people.Add(new Person { FirstName = "Kenny", LastName = "York" });
+            //people.Add(new Person { FirstName = "Kacey", LastName = "York" });
+            //people.Add(new Person { FirstName = "Doug", LastName = "Porter" });
+            //people.Add(new Person { FirstName = "Casey", LastName = "Kuluz" });
+            
+            
+            //people.ListChanged += new ListChangedEventHandler(people_ListChanged);
         }
+
+        void people_ListChanged(object sender, ListChangedEventArgs e)
+        {
+            
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //load existing
+
+            //txtFirst.DataBindings.Add("Text", people[0], "FirstName");
+            //txtLast.DataBindings.Add("Text", people[0], "LastName");
+            //people.AllowEdit = false;    
+            activePerson = new Person();
+            txtFirst.DataBindings.Add("Text", activePerson, "FirstName");
+            txtLast.DataBindings.Add("Text", activePerson, "LastName");
+
+            Update();
+        }
+
+        private void btnPrev_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            if (btnEdit.Text == "Add")
+            {
+                AddNew();
+            }
+            else
+            {
+                //EditCurrent();
+            }
+        }
+
+        private void AddNew()
+        {
+            people.Add(activePerson);
+            ++active;
+            Update();
+        }
+
+        private void Update()
+        {
+            btnPrev.Enabled = (active > 0);
+            btnNext.Enabled = (active < (people.Count-1));
+
+            if (active >= 0 && active < (people.Count - 1))
+            {
+                activePerson = new Person { FirstName = people[active].FirstName, LastName = people[active].LastName };
+            }
+            else
+            {
+                activePerson = new Person();
+            }
+
+            ((PropertyManager)this.BindingContext[activePerson]).ResumeBinding();
+
+            if (active == people.Count)
+            {
+                btnEdit.Text = "Add";
+            }
+            else
+            {
+                btnEdit.Text = "Edit";
+            }
+        }
+    }
+
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
     }
 }
