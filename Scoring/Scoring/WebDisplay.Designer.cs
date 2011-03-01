0795e26bc368fe0f8733e34e61c5302d40381217
Kenny York <kenny.york+github@gmail.com>
2011-02-28 21:19:44 -0600

    HTML and printing stuff

---
+++ Scoring/Scoring/WebDisplay.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace Scoring
+{
+    partial class WebDisplay
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(WebDisplay));
+            this.webBrowser = new System.Windows.Forms.WebBrowser();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // webBrowser
+            // 
+            this.webBrowser.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.webBrowser.Location = new System.Drawing.Point(0, 28);
+            this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.Size = new System.Drawing.Size(900, 702);
+            this.webBrowser.TabIndex = 0;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(900, 25);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton1.Text = "toolStripButton1";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // WebDisplay
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(900, 730);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.webBrowser);
+            this.Name = "WebDisplay";
+            this.Text = "WebDisplay";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+    }
+}
---
+++ Scoring/Scoring/WebDisplay.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Web.UI.HtmlControls;
+using Microsoft.Win32;
+using System.IO;
+using System.Threading;
+
+namespace Scoring
+{
+    public partial class WebDisplay : Form
+    {
+        public WebDisplay()
+        {
+            InitializeComponent();            
+        }
+
+        private string lastOutput;
+
+        private const string HTML_BASE = @"<html><body>{0}</body></html>";
+        private const string TABLE = @"<table class=""{0}"">{1}</table>";
+
+        private const string ROUND_HEADER = @"<tr><td class=""round_number"">Round Number</td><td class=""round_red"">Red</td><td class=""round_green"">Green</td><td class=""round_blue"">Blue</td><td class=""round_yellow"">Yellow</td></tr>";
+        private const string ROUND_ROW_COLOR = @"<tr><td class=""round_number"">{0}</td><td class=""round_red"">{1}</td><td class=""round_green"">{2}</td><td class=""round_blue"">{3}</td><td class=""round_yellow"">{4}</td></tr>";
+        private const string ROUND_ROW_NO_COLOR = @"<tr><td class=""round_number"">{0}</td><td class=""round_plain"">{1}</td><td class=""round_plain"">{2}</td><td class=""round_plain"">{3}</td><td class=""round_plain"">{4}</td></tr>";
+
+        public void RoundDisplay(List<Round> rounds, bool withColor)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(ROUND_HEADER);
+
+            string template = withColor ? ROUND_ROW_COLOR : ROUND_ROW_NO_COLOR;
+            foreach (var r in rounds)
+            {
+                //sb.AppendFormat(ROUND_ROW, r.Number, r.Red, r.Green, r.Blue, r.Yellow);
+                sb.AppendFormat(template, r.Number, r.Red.Name, r.Green.Name, r.Blue.Name, r.Yellow.Name);
+            }
+
+            string table = string.Format(TABLE, "round_table", sb.ToString());
+            string document = string.Format(HTML_BASE, table);
+            WriteDocument(document);
+        }
+
+        private const string TEAM_ROUND_HEADER = @"<tr><td class=""round_number"">Round Number</td><td class=""round_color"">Color</td></tr>";
+        private const string TEAM_ROUND_ROW = @"<tr><td class=""round_number"">{0}</td><td class=""round_{1}"">{2}</td></tr>";
+
+        public void TeamRoundDisplay(Team team)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(TEAM_ROUND_HEADER);
+
+            foreach (var r in team.Rounds)
+            {
+                string color = r.TeamColor(team);
+                sb.AppendFormat(TEAM_ROUND_ROW, r.Number, color.ToLower(), color);
+            }
+
+            string table = string.Format(TABLE, "tean_round_table", sb.ToString());
+            string document = string.Format(HTML_BASE, table);
+            WriteDocument(document);
+        }
+
+        private void WriteDocument(string text)
+        {            
+            webBrowser.DocumentText = text;
+            lastOutput = text;
+        }
+
+        public void WriteHTML(string path)
+        {
+            DateTime t = DateTime.Now;
+            
+            try
+            {
+                File.WriteAllText(path, lastOutput);
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public void QuickPrint()
+        {
+            while (webBrowser.ReadyState != WebBrowserReadyState.Complete)
+            {
+                Application.DoEvents();
+            }
+
+            //Show();
+            //Visible = false;
+            Print();
+            //Hide();
+            //Close();
+        }
+
+        public void Print()
+        {
+            DateTime t = DateTime.Now;
+
+            //reset.WaitOne();
+
+            if (webBrowser.ReadyState != WebBrowserReadyState.Complete)
+            {
+                return;
+            }
+
+            string keyName = @"Software\Microsoft\Internet Explorer\PageSetup";
+            using (RegistryKey key = Registry.CurrentUser.OpenSubKey(keyName, true))
+            {
+                if (key != null)
+                {
+                    string old_footer = (string)key.GetValue("footer");
+                    string old_header = (string)key.GetValue("header");
+                    key.SetValue("footer", "");
+                    key.SetValue("header", "");
+                    //webBrowser.ShowPrintPreviewDialog();
+                    webBrowser.Print();      
+                    key.SetValue("footer", old_footer);
+                    key.SetValue("header", old_header);
+                }
+            }  
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            Print();   
+        }
+    }
+}
